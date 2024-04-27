25b51e5674cba63fa6c20f4e2a167336589e971a
DQesgjs <65653560+yuansh3354@users.noreply.github.com>
2024-04-28 00:36:24 +0800

    Update and rename Step.04.Analysis_Epithelium.R to Step.03.Analysis_Epithelium.R

--- Step.03.Analysis_Epithelium.R
+++ Step.03.Analysis_Epithelium.R
@@ -1,13 +1,5 @@
-setwd("/Volumes/BANQ/")
-rm(list = ls())
-gc(reset = T)
-source('script/00.Functions.R')
-# unload packages
-pkgs[which(load.packages == F)]
-########################## Step.04 Epithelium ##########################
+########################## Step.03.1 UMAP & Markers ##########################
 epi = readRDS('result/RDS/03.1.Epithelium.RDS')
-
-########################## Step.04.1 UMAP & Markers ##########################
 sce = epi
 
 # UMAP
@@ -15,8 +7,8 @@ DimPlot(sce, group.by = 'cancers',
         cols = epi.colors,
         raster = F,order = T) + NoLegend()+
   theme(
-    axis.title = element_blank(),  #轴标题
-    axis.text = element_blank(), # 文本
+    axis.title = element_blank(),  
+    axis.text = element_blank(), 
     axis.ticks = element_blank(),
     axis.line = element_blank()) + ggtitle('')
 # ggsave('plts/umap-epi.pdf',height = 4,width = 4)
@@ -57,15 +49,6 @@ x = ggplot(count_df, aes(x = reorder(Subtype,Total),fill=Subtype)) +
 x
 
 # Feature plots 
-CellMarkers = list(
-  'Prolification-like' =  c(unlist(strsplit('CD24,TUBB3,TCIM',','))),
-  'Neuroendocrine-like' = c(unlist(strsplit('GPHN,GFRA1,NEAT1',','))),
-  "Metastasis-like" = c(unlist(strsplit('RNF181,MDK,BST2',','))),
-  'Invasion-like' = c('MKI67','TOP2A','NDC80'),
-  'Immune-like' = c(unlist(strsplit('CD74,PTPRC,PARP14',','))),
-  'EMT-like' = c(unlist(strsplit('VIM,FN1,MMP2',','))),
-  c(unlist(strsplit(paste(grep('^ASCL',rownames(sce),value = T),collapse = ','),',')))
-)
 x = jjDotPlot(object = epi,
               gene = unlist(CellMarkers),
               xtree = F,
@@ -77,18 +60,9 @@ x = jjDotPlot(object = epi,
               tile.geom = T)
 x
 
-########################## Step.04.2 dyno ##########################
+########################## Step.03.2 dyno ##########################
 library(dyno)
 library(tidyverse)
-
-# Select Local samples and sampling 20000 cells for calculated pseudo-time
-use.cells = sce@meta.data[which(sce$Type =='Local'),] %>% rownames() 
-sce = subset(sce, cells = use.cells)
-Idents(sce) = sce$Subtype
-
-use.cells = sample(Cells(sce),20000)
-sce = subset(sce, cells = use.cells)
-
 dataset <- wrap_expression(
   expression = t(sce@assays$RNA$data),
   counts = t(sce@assays$RNA$counts)
@@ -116,7 +90,7 @@ plot_dendro(model,  size_cells = 1,
             grouping =sce$Subtype) + 
   scale_color_manual(values =epi.colors ) + NoLegend()
 
-########################## Step.04.3 Cell Distribution ##########################
+########################## Step.03.3 Cell Distribution ##########################
 sce = epi
 
 meta.data = sce@meta.data
@@ -161,24 +135,6 @@ distribution_Roe(
 )
 ggsave2('plts/roe.pdf',height = 6,width = 4)
 
-# ShannonEntropy
-estimateShannonEntropy <- function(seurat_object){
-  seurat_object <- FindNeighbors(seurat_object, k.param=30, return.neighbor=T)
-  neighbors <- seurat_object@neighbors$RNA.nn@nn.idx
-  cell_names <- seurat_object@neighbors$RNA.nn@cell.names
-  sample <- seurat_object$Type
-  entropy <- apply(neighbors, 1, function(x){
-    data <- table(sample[x])
-    if(length(unique(sample[x]))==1){
-      percent <- 1
-    }else{
-      percent <- prop.table(data)
-    }
-    cell_entropy <- -1*sum(percent*log2(percent))
-  })
-  return(entropy)
-}
-
 for (cell.type in unique(Idents(sce))) {
   temp = subset(sce,idents=cell.type)
   temp$ShannonEntropy = estimateShannonEntropy(temp)
@@ -193,110 +149,7 @@ for (cell.type in unique(Idents(sce))) {
     theme(legend.position = "none")
 }
 
-########################## Step.04.4 Markers ##########################
-########################## Step.04.5 GSEA ##########################
-sce = epi
-degs.std = FindAllMarkers(sce)
-lapply(seq_along(unique(degs.std$cluster)), function(x){
-  tmp <- degs.std |> 
-    dplyr::filter(cluster == unique(degs.std$cluster)[x]) |>
-    dplyr::arrange(desc(avg_log2FC))
-  
-  # gene list
-  glist <- tmp$avg_log2FC
-  names(glist) <- tmp$gene
-  
-  # enrichment
-  ego <- clusterProfiler::gseGO(geneList = glist,
-                                ont = "BP",
-                                OrgDb = org.Hs.eg.db,
-                                keyType = "SYMBOL",
-                                pvalueCutoff = 0.05)
-  
-  # to data.frame
-  df <- data.frame(ego) |> 
-    dplyr::arrange(pvalue,desc(NES))
-  # df = df[which(df$NES > 0),]
-  
-  
-  return(ego)
-}) -> GSEA.list
-names(GSEA.list) = unique(degs.std$cluster)
-
-# Get Module for each Subtypes
-C1 = c('mesenchyme development','angiogenesis')
-C2 = c('negative regulation of immune response','negative regulation of T cell activation')
-C3 = c("chromosome segregation","nuclear chromosome segregation")
-C4 = c("positive regulation of cell migration","positive regulation of cell motility")
-C5 = c('regulation of circadian rhythm','synaptic vesicle exocytosis')
-C6 = c("organelle fission","nuclear division")
-
-lapply(seq_along(names(GSEA.list)), function(x){
-  
-  # to data.frame
-  df <- data.frame(GSEA.list[[x]]) |> 
-    dplyr::arrange(pvalue,desc(NES))
-  # df = df[which(df$NES > 0),]
-  
-  # plot
-  lapply(c(1:2), function(y){
-    GseaVis::gseaNb(object = GSEA.list[[x]],
-                    subPlot = 2,
-                    geneSetID = df[which(df$Description %in% get(paste0('C',x))),]$ID[y],
-                    addPval = T,
-                    pvalX = 0.9,
-                    pvalY = 0.8)
-  }) -> plist
-  
-  # combine
-  pplist <- cowplot::plot_grid(plotlist = plist,nrow = 1,align = 'hv')
-  
-  return(pplist)
-}) -> gglist
-
-x1 = gglist[[1]]
-x2 = gglist[[2]]
-x3 = gglist[[3]]
-x4 = gglist[[4]]
-x5 = gglist[[5]]
-x6 = gglist[[6]]
-
-topptx(x1,'plts/EMT-GSEA.pptx',width = 8,height = 4)
-topptx(x2,'plts/Immune-GSEA.pptx',width = 8,height = 4)
-topptx(x3,'plts/Invasion-GSEA.pptx',width = 8,height = 4)
-topptx(x4,'plts/Metastasis-GSEA.pptx',width = 8,height = 4)
-topptx(x5,'plts/Neuroendocrine-GSEA.pptx',width = 8,height = 4)
-topptx(x6,'plts/Prolification-GSEA.pptx',width = 8,height = 4)
-
-features = c(C1,C2,C3,C4,C5,C6)
-GSEA.list = lapply(GSEA.list, as.data.frame)
-
-df1 = GSEA.list$`EMT-like`[which(GSEA.list$`EMT-like`$Description%in%features),c('Description','NES')]
-df2 = GSEA.list$`Immune-like`[which(GSEA.list$`Immune-like`$Description%in%features),c('Description','NES')]
-df3 = GSEA.list$`Invasion-like`[which(GSEA.list$`Invasion-like`$Description%in%features),c('Description','NES')]
-df4 = GSEA.list$`Metastasis-like`[which(GSEA.list$`Metastasis-like`$Description%in%features),c('Description','NES')]
-df5 = GSEA.list$`Neuroendocrine-like`[which(GSEA.list$`Neuroendocrine-like`$Description%in%features),c('Description','NES')]
-df6 = GSEA.list$`Prolification-like`[which(GSEA.list$`Prolification-like`$Description%in%features),c('Description','NES')]
-colnames(df1) = c('Description','df1') 
-colnames(df2) = c('Description','df2')
-colnames(df3) = c('Description','df3')
-colnames(df4) = c('Description','df4')
-colnames(df5) = c('Description','df5')
-colnames(df6) = c('Description','df6')
-df = join_all(list(df1,df2,df3,df4,df5,df6),by = 'Description',type = 'full')
-rownames(df) = df[,1]
-df = df[,-1]
-df = df[features,]
-colnames(df) = names(GSEA.list)
-# df[is.na(df)] = -5
-x = pheatmap::pheatmap(df, na_col = 'grey',
-                       cluster_rows = F,
-                       cluster_cols = F,
-                       number_format = '%.1f',number_color = 'black',
-                       cellwidth = 18,cellheight = 18,display_numbers = T,
-                       color =colorRampPalette(c("navy", "white", "firebrick3"))(64) )
-
-########################## Step.04.6 CellCycleScoring ##########################
+########################## Step.03.4 CellCycleScoring ##########################
 sce = epi
 hg.pairs <- readRDS(system.file("exdata", "human_cycle_markers.rds", package="scran"))
 
@@ -343,7 +196,7 @@ x = ggplot(celltype_counts, aes(x = "", y = percentage, fill = Phase, label = pa
   theme(legend.position = "right") +
   scale_fill_npg()
 
-########################## Step.04.7 GenoType ##########################
+########################## Step.03.5 GenoType ##########################
 # SComatic
 if(T){
   file.list = dir(pattern = '_cellranger710')
@@ -386,11 +239,6 @@ if(T){
     print(SampleID)
     assign(SampleID,mut.ids)
   }
-  
-  SComatic = rbind(`CA-10-1`,`CA-10-2`,`CA-11-1`,`CA-11-2`,`CA1225-1`,`CA1225-2`,
-                   `LN-10`,`LN-3-0913`,`M301_5T`,`MBDJ_2T`,`MLBA_1P`,`MLBA_1T`,
-                   `MLYJ_4T`,`MLYZ_3L`,`MLYZ_3T`,`mQD2-P`,`mQD2-T`,`N-3-0913`,
-                   `P-10`,`T-3-0913`) %>% as.data.frame()
   SComatic = SComatic[which(SComatic$Biotype =='protein_coding'),]
   SComatic = SComatic[which(!(SComatic$Type %in% c('synonymous_variant','splice_region_variant&synonymous_variant'))),]
   saveRDS(SComatic,file = 'result/RDS/scomatic.rds')
@@ -414,14 +262,6 @@ x = ggplot(result, aes(x = Count, y =Cell_types , fill = Cell_types)) +
   theme_bw() 
 x
 
-CellMarkers = list(
-  'Prolification-like' =  c(unlist(strsplit('CD24,TUBB3,TCIM',','))),
-  'Neuroendocrine-like' = c(unlist(strsplit('GPHN,GFRA1,YAP1',','))),
-  "Metastasis-like" = c(unlist(strsplit('RNF181,MDK,BST2',','))),
-  'Invasion-like' = c('MKI67','TOP2A','NDC80'),
-  'Immune-like' = c(unlist(strsplit('CD74,PTPRC,PARP14',','))),
-  'EMT-like' = c(unlist(strsplit('VIM,FN1,MMP2',',')))
-)
 result <- df %>%
   group_by(Cell_types,Gene) %>%
   summarise(Count = log(n(),2))
@@ -454,7 +294,6 @@ result
 result = result[which(result$Gene %in% c('TP53','UTY','NRS',
                                           'CTNNB1','PTEN','ATM','CDH1',
                                           'GATA3','MAP3K1','HERC2','KRAS','ERBB2')),]
-# 使用 ggplot 绘制柱状图，并添加折线、点和红色标记
 ggplot(result, aes(Gene, Count,fill=Cell_types)) + 
   geom_bar(stat = 'identity',width = 0.75,position = 'dodge') + 
   # geom_line(aes(group = Cell_types), color = 'blue') +  # 添加折线
@@ -468,72 +307,3 @@ ggplot(result, aes(Gene, Count,fill=Cell_types)) +
         legend.position = "none"  
   ) + 
   scale_fill_manual(values = epi.colors)
-
-
-
-
-
-# Numbat
-if(F){
-  for(ids in dir('result/OUT_Numbat',full.names = T)){
-    num_files <- length(list.files(ids))
-    if(num_files > 50){
-      print(str_split(ids,'/',simplify = T)[3])
-      temp =  Numbat$new(out_dir = ids)
-      temp$clone_post$cell = paste0(sub('_cellranger710','',str_split(ids,'/',simplify = T)[3]),'_',
-                                    temp$clone_post$cell)
-      temp$joint_post$cell = paste0(sub('_cellranger710','',str_split(ids,'/',simplify = T)[3]),'_',
-                                    temp$joint_post$cell)
-      assign(str_split(ids,'/',simplify = T)[3],temp)
-      rm(temp)
-    }
-  }
-  
-  Join_post = data.frame(cell = character(), CHROM = character(), cnv_state = character(),Score = numeric(),stringsAsFactors = FALSE)
-  segs = NULL
-  for (ids.n in ls(pattern = '_cellranger710$')) {
-    print(ids.n)
-    ids = get(ids.n)
-    ids.feature = c("cell","CHROM","cnv_state",paste0('Z_',unique(ids$joint_post$cnv_state)))
-    joint.pos = ids$joint_post[,..ids.feature]
-    
-    result = NULL
-    for (stat in unique(joint.pos$cnv_state)) {
-      print(stat)
-      temp = subset(joint.pos, cnv_state == stat, select = c("cell", "CHROM", "cnv_state", paste0('Z_',stat)))
-      colnames(temp) = c("cell","CHROM","cnv_state", "Score")
-      result = rbind(result,temp)
-    }
-    
-    Join_post = rbind(Join_post,result)
-    seg = ids$segs_consensus
-    
-    seg = ids$bulk_clones %>%
-      mutate(cnv_state = cnv_state_post) %>%
-      distinct(CHROM, seg, cnv_state, seg_start, seg_end) %>%
-      filter(cnv_state != 'neu') %>%
-      mutate(length = seg_end - seg_start)
-    seg$SampleID = ids.n
-    segs = rbind(segs,seg)
-  }
-  
-  ggplot(Join_post,aes(CHROM, Score)) +
-    geom_jitter() + 
-    facet_wrap(~cnv_state)
-  
-  
-  
-  segs = segs %>%
-    mutate(group = SampleID) 
-  ids = names(table(segs$SampleID))[table(segs$SampleID)>15]
-  segs = segs[which(segs$SampleID %in% ids),]
-  x=segs%>%
-    cnv_heatmap() +
-    theme(
-      axis.title.y = element_text(size = 8, hjust = -5),
-      plot.margin = margin(0, 0, 0, 0.5, unit = 'mm')
-    )
-  topptx(x,'plts/Numbat.pptx',width = 18)
-  
-}
-
